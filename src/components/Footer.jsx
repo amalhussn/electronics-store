195797262fb1fcf017e958dff67114d3c7645b2c
amalhussn <amalohussein@gmail.com>
2025-07-06 06:25:06 -0400

    Fixed href warnings in Footer for Netlify build

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,34 +5,33 @@ export default function Footer() {
         <div className="flex flex-col gap-6 px-5 py-10 text-center @container">
           {/* Footer Links */}
           <div className="flex flex-wrap items-center justify-center gap-6 @[480px]:flex-row @[480px]:justify-around">
-            <a className="text-[#49689c] text-base font-normal leading-normal min-w-40" href="#">
+            <a className="text-[#49689c] text-base font-normal leading-normal min-w-40" href="/">
               About Us
             </a>
-            <a className="text-[#49689c] text-base font-normal leading-normal min-w-40" href="#">
+            <a className="text-[#49689c] text-base font-normal leading-normal min-w-40" href="/">
               Contact
             </a>
-            <a className="text-[#49689c] text-base font-normal leading-normal min-w-40" href="#">
+            <a className="text-[#49689c] text-base font-normal leading-normal min-w-40" href="/">
               Privacy Policy
             </a>
-            <a className="text-[#49689c] text-base font-normal leading-normal min-w-40" href="#">
+            <a className="text-[#49689c] text-base font-normal leading-normal min-w-40" href="/">
               Terms of Service
             </a>
           </div>
 
           {/* Social Icons (Placeholder Links for Now) */}
           <div className="flex flex-wrap justify-center gap-4">
-            <a href="#">
+            <a href="/">
               <div className="text-[#49689c]">
-                {/* Replace below with actual icons later */}
                 <span className="sr-only">Twitter</span>
               </div>
             </a>
-            <a href="#">
+            <a href="/">
               <div className="text-[#49689c]">
                 <span className="sr-only">Facebook</span>
               </div>
             </a>
-            <a href="#">
+            <a href="/">
               <div className="text-[#49689c]">
                 <span className="sr-only">Instagram</span>
               </div>
